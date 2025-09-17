c8b41f14938ecb0b29467644232e796209ed0309
victor-elite <vkosiso668@gmail.com>
2025-09-17 11:06:48 +0100

    improved intro statement to make it meaningful

--- scheme/variables2.js
+++ scheme/variables2.js
@@ -2,7 +2,7 @@ const student_name="victor"
 const course="computer"
 const lecturer="boniface"
 
-const intro= student_name+""+course+""+lecturer
+const intro= student_name+" i study "+course+" my lecturer is "+lecturer
 
 
 console.log("my name is",intro)
