de6f06f9895efcd8888bde836a883ae1902e5eef
victor-elite <vkosiso668@gmail.com>
2025-09-17 10:47:04 +0100

    first commit

---
+++ scheme/variables2.js
@@ -0,0 +1,8 @@
+const student_name="victor"
+const course="computer"
+const lecturer="boniface"
+
+const intro= student_name+""+course+""+lecturer
+
+
+console.log("my name is",intro)
---
+++ variables.js
@@ -0,0 +1,25 @@
+const name = "boniface"
+let score = 300
+var course_title = "introduction to computer"
+const passed = false
+let average_score = 4.8
+
+if (!passed) {
+    let new_passed = true
+    const new_name = "sixtus"
+    var new_socre = 500
+
+    console.log("new passed= ", new_passed)
+    console.log("new name= ", new_name)
+    console.log("new socre= ", new_socre)
+}
+
+
+console.log("outside socre= ", new_socre)
+
+
+console.log(typeof (name))
+console.log(typeof (score))
+console.log(typeof (course_title))
+console.log(typeof (passed))
+console.log(typeof (average_score))
